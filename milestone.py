2eed2745902e22a35ac63a9c1134fbcc14140ed5
Michael Painter <michaelpainter.1994@gmail.com>
2016-12-11 22:54:42 -0800

    Broken feature selection???

--- milestone.py
+++ milestone.py
@@ -28,10 +28,11 @@ for i in range(760):
 [testX, testY] = util.createTestData(testYears, order)
 winBoundary = 0.5
 lossBoundary = -0.5
-util.linearClassification(trainX, trainY, testX, testY, winBoundary, lossBoundary)
-util.SVC(trainX, trainY, testX, testY)
-util.layeredSVM(trainX, trainY, testX, testY)
-util.linearSVC(trainX, trainY, testX, testY)
+print "FINISHED EXTRACTING TRAINING DATA"
+#util.linearClassification(trainX, trainY, testX, testY, winBoundary, lossBoundary)
+#util.SVC(trainX, trainY, testX, testY)
+#util.layeredSVM(trainX, trainY, testX, testY)
+#util.linearSVC(trainX, trainY, testX, testY)
 #util.NB(trainX, trainY, testX, testY)
 '''
 iteration = 0
@@ -41,3 +42,89 @@ for (av, result) in a.attributeVectorIterator(trainYears):
     if iteration == 1:
         break
 '''
+
+
+kValidationRatio = 0.3
+def forwardSelection(origTrainX, origTrainY, testX, testY, order, trainingAlgorithm):
+    """
+    Performs forward selection 
+
+    :param origTrainX: original training set x values
+    :param origTrainY: original training set y values
+    :param testX: test set x values
+    :param textY: test set y values
+    :param order: mapping from attributes to indexes into the feature vectors
+    :param trainingAlgorithm: the training algorithm we're using, takes training and test sets, returns test error
+    """
+    (trainX, trainY, valX, valY) = splitData(origTrainX, origTrainY, kValidationRatio)
+    featuresSelected = []
+    remainingFeatures = order.keys()
+    totalNumFeatures = len(trainX[0])
+    for i in range(totalNumFeatures):
+        bestFeature = None
+        bestFeatureError = float('inf')
+        for feature in remainingFeatures:
+            features = featuresSelected + [feature]
+            selTrainX = selectFeatures(trainX, features, order) # selTrainX for selectedFeatures of trainX
+            selTestX = selectFeatures(valX, features, order)
+            error = trainingAlgorithm(selTrainX, trainY, selValX, valY) # try this subset of features, and how well it does on validation set
+            if error < bestFeatureError:
+                bestFeature = feature
+                bestFeatureError = error
+        featuresSelected = featuresSelected + [bestFeature]
+        print str(i) + "th feature selected: " + bestFeature
+        print "----> error:" + bestFeatureError
+
+    # the ith subset selected by forward selection is featuresSelected[:i] (i running from 1 to numFeratures)
+    # now we try each of the subsets on the algorithm, training with the whole (original) training set, and test on final test set
+    testErrors = []
+    for i in range(totalNumFeatures):
+        features = featuresSelected[:i+1]
+        selTrainX = selectFeatures(origTrainX, features, order)
+        selTestX = selectFeatures(testX, features, order)
+        error = trainingAlgorithm(selTrainX, origTrainY, selTestX, testY)
+        testErrors.append(error)
+
+    # Return the order of features selected and the errors of the respective subsets
+    return (featuresSelected, testErrors)
+
+def splitData(xs, ys, ratio):
+    """
+    Split input feature vectors and targets into a training set and validation set
+    with a ratio of (1-ratio):ratio of training:validation
+
+    :param xs: The feature vectors
+    :param ys: The targets
+    :param ratio: The ratio to split the set up by
+    """
+    trainLen = int(len(xs) * (1 - ratio))
+    return (xs[:trainLen], ys[:trainLen], xs[trainLen:], ys[trainLen:])
+
+def selectFeatures(xs, features, order):
+    """
+    Takes a set of feature fectors 'xs', and reduces their dimensionality to only 
+    include the subset of features, 'features'.
+
+    :param xs: A set of feature vectors
+    :param features: A subset of attributes to select in the feature vectors
+    :param order: A mapping from attributes to indexes in the feature vectors
+    """
+    newXs = []
+    for x in xs:
+        newX = []
+        for feature in features:
+            newX.append(order[feature])
+        newXs.append(newX)
+    return newXs
+
+
+
+(forwardSelectedFeatures, errors) = forwardSelection(trainX, trainY, testX, testY, order, util.linearSVC)
+print "\n"
+print "\n"
+print forwardSelectedFeatures
+print "\n"
+print "\n"
+print errors
+print "\n"
+print "\n"
